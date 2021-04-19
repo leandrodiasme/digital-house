9ad9ae06e5c1518a285e56412b67fbfa5d154e08
Leandro Dias <leandrodias.me@gmail.com>
2021-04-19 10:14:37 -0400

    sending

--- backend/controllers/LegendariesController.js
+++ backend/controllers/LegendariesController.js
@@ -14,10 +14,29 @@ const controller = {
         const {name, description, type, healthPoints, specialAttack, defense, attack, experience, specialDefense}
         = req.body;
 
+        if(!description){
+            return res.status(400).json({error: "Description is required"});
+        }
+
         const legendary = LegendariesService.createLegendary(name, description, type, healthPoints, specialAttack, defense, attack, experience, specialDefense);
 
         res.json(legendary);
         console.log(req.body)
+    },
+
+    update: (req, res)=>{
+        const {id} = req.params;
+
+        const {name, description, type, healthPoints, specialAttack, defense, attack, experience, specialDefense}
+        = req.body;
+
+        if(!description){
+            return res.status(400).json({error: "Description is required"});
+        }
+
+        // Aqui a gente vai chamar o método do service que sobreescreve
+
+        res.json();
     }
 }
 
--- backend/routes/legendaries.js
+++ backend/routes/legendaries.js
@@ -7,4 +7,6 @@ router.get('/', controller.index);
 
 router.post('/', controller.create);
 
+router.put('/id', controller.update);
+
 module.exports = router;
